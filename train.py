12c2003174fe77281bb6da779ed6a359917529f1
haochenye <21724054@zju.edu.cn>
2021-07-09 22:47:58 +0800

    fix small bug

--- train.py
+++ train.py
@@ -380,7 +380,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
                         iter_time = interval_time / opt.interval
 
                         mloss = mloss / opt.interval
-                        s = f"YoloV5: epoch{epoch}" \
+                        s = f"YoloV5: epoch[{epoch}] " \
                             f"lbox: {i}/{nb} {mloss[0].item() * opt.batch_size:2f}, " \
                             f"lobj: {mloss[1].item() * opt.batch_size:2f}, " \
                             f"lcls: {mloss[2].item() * opt.batch_size:2f} " \
